71a06455c25c083d7f2a9bf1e76dfa04e787eb75
10576 <a10576@SKCC25N00767.local>
2026-03-12 14:19:57 +0900

    aks 기본 배포까지 완료

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -44,3 +44,49 @@ module "subnet_private_endpoint" {
   address_prefixes = ["10.10.2.0/24"]
 
 }
+
+# module "nat_gateway" {
+#   source = "../../modules/natgateway"
+
+#   nat_gateway_name    = "dev-nat"
+#   public_ip_name      = "dev-nat-ip"
+#   resource_group_name = module.rg.name
+#   location            = var.location
+#   subnet_id           = module.subnet.id
+# }
+
+# module "route_table" {
+#   source = "../../modules/routetable"
+
+#   route_table_name    = "dev-aks-rt"
+#   location            = var.location
+#   resource_group_name = module.rg.name
+#   subnet_id           = module.subnet.subnet_id
+
+#   routes = {
+#     default = {
+#       address_prefix = "0.0.0.0/0"
+#       next_hop_type  = "VirtualAppliance"
+#       next_hop_ip    = "10.0.100.4"
+#     }
+#   }
+# }
+
+module "kubernetes" {
+  source = "../../modules/kubernetes"
+
+  cluster_name = "koo-aks"
+  
+  system_node_pool = {
+    name       = "devnp"
+    vm_size    = "Standard_D4ds_v5"
+    node_count = 1
+    min_count  = null
+    max_count  = null
+  }
+  dns_prefix = "aksdns"
+  location            = var.location
+  resource_group_name = module.rg.name
+  subnet_id           = module.subnet_aks.subnet_id
+  
+}
