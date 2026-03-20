12bd1eb6ee17b5e1afcb4f4a1ce318104da10026
10576 <a10576@SKCC25N00767.local>
2026-03-20 14:16:32 +0900

    AWS bootstrap 구조 잡기

---
+++ AWS/bootstrap/main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "platform-tfstate-koo"  # globally unique 해야됨
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name         = "platform-tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
